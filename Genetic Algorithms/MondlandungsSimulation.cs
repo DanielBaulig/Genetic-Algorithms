bdecafba93b5d2ef5a50085440984d60b90d856c
unknown <Chrizz@.(none)>
2010-01-06 15:59:24 +0100

    MondlandungsSimulation.cs

---
+++ Genetic Algorithms/MondlandungsSimulation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticAlgorithms
+{
+    class MondlandungsSimulation
+    {
+        private int startHoehe;
+        private int startTreibstoff;
+        private int raumschiffGewicht;
+
+        public MondlandungsSimulation(int raumschiffStartHoehe, int raumschiffStartTreibstoff, int raumschiffStartGewicht)
+        {
+            startHoehe = raumschiffStartHoehe;
+            startTreibstoff = raumschiffStartTreibstoff;
+            raumschiffGewicht = raumschiffStartGewicht;
+            
+        }
+
+        public float ComputeFitness(ArrayList genes)
+        {
+            float fitness;
+            Raumfahrer pilot = new Raumfahrer(genes);
+            Raumschiff raumschiff = new Raumschiff(pilot, startHoehe, startTreibstoff, raumschiffGewicht);
+            pilot.SteuereRaumschiff(raumschiff);
+            while (raumschiff.Hoehe >= 0)
+            {
+                raumschiff.SimuliereRunde();
+            }
+
+            fitness = (1000 - raumschiff.Geschwindigkeit) / 1000 * 0.9 + raumschiff.Treibstoff / startTreibstoff * 0.1;
+
+        }
+    }
+}
